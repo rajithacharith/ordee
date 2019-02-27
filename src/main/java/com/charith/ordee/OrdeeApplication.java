bc81ca117ff97e671c8d6885216cbbd8e447a51e
lakmali <piyarathnalakmali@gmail.com>
2019-02-27 23:17:03 +0530

    initial Commit

---
+++ src/main/java/com/charith/ordee/OrdeeApplication.java
@@ -0,0 +1,13 @@
+package com.charith.ordee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdeeApplication.class, args);
+    }
+
+}
