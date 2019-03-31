7537da99e227e6915b10da31fba61e2238d94795
lakmali <piyarathnalakmali@gmail.com>
2019-03-31 18:44:04 +0530

    add customer controller and merchant controller test classes

---
+++ src/test/java/com/charith/ordee/controller/CustomerControllerTest.java
@@ -0,0 +1,13 @@
+package com.charith.ordee.controller;
+
+import org.junit.Before;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+public class CustomerControllerTest extends AbstractTest {
+
+    @Before
+    public void setup(){
+        super.setUp();
+    }
+}
---
+++ src/test/java/com/charith/ordee/controller/MerchantControllerTest.java
@@ -0,0 +1,12 @@
+package com.charith.ordee.controller;
+
+import org.junit.Before;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+public class MerchantControllerTest extends AbstractTest {
+    @Before
+    public void setup(){
+        super.setUp();
+    }
+}
