85459d1529226de434c27d8b6af60e607ef66945
lakmali <piyarathnalakmali@gmail.com>
2019-04-08 22:01:21 +0530

    add storage service

--- src/main/java/com/charith/ordee/OrdeeApplication.java
+++ src/main/java/com/charith/ordee/OrdeeApplication.java
@@ -1,13 +1,25 @@
 package com.charith.ordee;
 
+import com.charith.ordee.services.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class OrdeeApplication {
+public class OrdeeApplication implements CommandLineRunner {
+    @Resource
+    StorageService storageService;
 
     public static void main(String[] args) {
+
         SpringApplication.run(OrdeeApplication.class, args);
+    }
+
+    @Override
+    public void run(String... arg) throws Exception {
+        storageService.init();
     }
 
 }
--- src/main/java/com/charith/ordee/rest/MerchantController.java
+++ src/main/java/com/charith/ordee/rest/MerchantController.java
@@ -3,14 +3,19 @@ package com.charith.ordee.rest;
 import com.charith.ordee.beans.dto.FoodItemDTO;
 import com.charith.ordee.services.FoodItemService;
 import com.charith.ordee.services.OrderService;
+import com.charith.ordee.services.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/merchant")
 public class MerchantController {
+    @Autowired
+    private StorageService storageService;
     @Autowired
     private FoodItemService foodItemService;
     @Autowired
@@ -24,9 +29,23 @@ public class MerchantController {
     public ResponseEntity changeFoodDetails(){
         return new ResponseEntity(HttpStatus.OK);
     }
-    @PostMapping("/addPhotos") //TODO :: Add storage service
-    public ResponseEntity addPhotos(){
-        return new ResponseEntity("HelloWorld",HttpStatus.OK);
+    @PostMapping(value = "/addPhotos", consumes = "multipart/form-data") //TODO :: Add storage service
+    public ResponseEntity addPhotos(@RequestParam("file") MultipartFile file){
+        String message = "";
+        try {
+
+            storageService.store(file);
+
+
+            message = "You successfully uploaded " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.OK).body(message);
+        } catch (Exception e) {
+            message = "FAIL to upload " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+        }
+
+
+
     }
 
     @GetMapping("/checkOrders")
